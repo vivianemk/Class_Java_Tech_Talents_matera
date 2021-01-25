4d5bc53d4db1cc1f009b32ee75769fc5daa4cd6c
vivianemk <kikulevmk@gmail.com>
2021-01-24 22:02:35 -0300

    ex03_04

---
+++ src/dia03/ex03_04.java
@@ -0,0 +1,26 @@
+//Escreva um algoritmo em Java que leia dois números inteiros e determine qual é o menor.
+//Escreva um algoritmo que determina o maior também.
+
+package dia03;
+
+import java.util.Scanner;
+
+public class ex03_04 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Digite numero a");
+	
+		int a = in.nextInt();
+		
+		System.out.println("Digite numero b");
+		
+		int b = in.nextInt();
+		
+		if(a <b ) {
+			System.out.println("a menor que b");
+		} else
+			System.out.println(" a é maior que b");
+		in.close();
+	}
+
+}
